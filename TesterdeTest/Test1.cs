5b97b3ce5b8e15bed58eb5847b09e35c104c21e4
Gerke-v-W <S1161016@student.windesheim.nl>
2022-09-25 20:43:47 +0200

    add test

---
+++ TesterdeTest/Test1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TesterdeTest
+{
+    public class Test1
+    {
+
+        [Fact]
+        public void SomeTest()
+        {
+            // Arrange
+
+
+            // Act
+
+
+            // Assert
+            Assert.Equal(1, 1);
+        }
+    }
+}
